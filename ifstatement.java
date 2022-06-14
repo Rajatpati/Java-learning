4de3db5156a54e906257c76685806c69649ecf8c
babulpati <babulpati786@gmail.com>
2022-06-14 12:32:22 +0530

    initial

---
+++ ifstatement.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ifstatement {
+    public static void main(String []args ){
+    Scanner scn=new Scanner (System.in);
+System.out.println("Enter the password ");
+int i=scn.nextInt();
+if (i==2021) {
+     System.out.println("Open to the System");}
+    }
+}
