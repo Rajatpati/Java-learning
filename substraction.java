1e529e281cb5d830f5d38414e363b84a8935979f
babulpati <babulpati786@gmail.com>
2022-06-14 12:16:56 +0530

    initial

---
+++ substraction.java
@@ -0,0 +1,9 @@
+public class substraction {
+    public static void main(String []args ){
+        int a =20;
+        int b=15;
+        int substraction= a-b;
+        System.out.println(substraction);
+    }
+    
+}
