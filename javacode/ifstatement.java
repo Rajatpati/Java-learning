94d622c7cb2cf752549cfaea12fe3fed6eae30fc
babulpati <babulpati786@gmail.com>
2022-06-14 13:15:53 +0530

    wakeup

--- javacode/ifstatement.java
+++ javacode/ifstatement.java
@@ -1,10 +1,13 @@
+package javacode;
+
 import java.util.Scanner;
 public class ifstatement {
     public static void main(String []args ){
-    Scanner scn=new Scanner (System.in);
-System.out.println("Enter the password ");
-int i=scn.nextInt();
-if (i==2021) {
-     System.out.println("Open to the System");}
+    try (Scanner scn = new Scanner (System.in)) {
+        System.out.println("Enter the password ");
+        int i=scn.nextInt();
+        if (i==2021) {
+             System.out.println("Open to the System");}
+    }
     }
 }
--- javacode/substraction.java
+++ javacode/substraction.java
@@ -1,3 +1,5 @@
+package javacode;
+
 public class substraction {
     public static void main(String []args ){
         int a =20;
