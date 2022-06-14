6111ebc2157502238dd7990ae37bfc8209df526a
babulpati <babulpati786@gmail.com>
2022-06-14 12:43:41 +0530

    initial

---
+++ elseifstatement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class elseifstatement {
+    public static void main(String[]args) {
+        Scanner scn=new Scanner (System.in);
+		System.out.println("Enter your persentage ");
+		int i=scn.nextInt();
+		if (i>=60) {
+			System.out.println("Fast divison");
+		}
+		else if (i>=50||i<60) {
+			System.out.println("Second divison");
+		}
+		else if (i<50||i>=35) {
+			System.out.println("Third divison");
+		}
+		else {
+			System.out.println("FAIL");
+		} 
+        
+    }
+}
