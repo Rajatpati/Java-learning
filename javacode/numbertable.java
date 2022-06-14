4a95b4f5b7386886d97c02803d81056b01d05725
babulpati <babulpati786@gmail.com>
2022-06-14 13:00:19 +0530

    number system

---
+++ javacode/numbertable.java
@@ -0,0 +1,15 @@
+package javacode;
+import java.util.Scanner;
+
+public class numbertable {
+    public static void main(String []args) {
+        int a,i;
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter any number :");
+		a=scn.nextInt();
+		for (i=1;i<=10;i++) {
+			System.out.println(a*i); 
+		}  
+    }
+    
+}
